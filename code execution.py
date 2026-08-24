e27e0a06094ca7b5340d728b1fc29695d34ddb6f
vishal aswani <vishalaswani7@vishals-MacBook-Air.local>
2026-08-24 20:49:01 +0530

    initial commit

---
+++ code execution.py
@@ -0,0 +1,36 @@
+print("this is day 1 of my coding journey and here is what i have learned:")
+
+# 1 variable : variable are used to store data that can be used and changed 
+age = 18 
+#here age is a variable and 18 is data
+name = vishal 
+#here name is variable and vishal is data
+
+# 2 input() is is used to take input from user 
+name = input("enter your name")
+print("your name is:" , name)
+
+# 3 conversion of string(text) into integer : whenever we use input() python stores the data as string
+age = int(input("what is your age"))
+#now if the user enter 18 it will take it as integer and not string and we can preform calculations
+age = int(input("what is your age"))
+print(age + 5 )
+
+# 4 expressions : it is a combination of operators and operands such as
+x = 10 (no expression)
+x + 3 (expression with 2 operands and 1 operator and a return value )
+
+# 5 statement : instructions that tells computer to proform an action such as
+print("hello world")
+x = 10
+name = input("what is your name")
+age = int(input("what is your age"))
+print("your name is :" , name)
+print("your age is :" , age)
+
+# 6 inside statement you can have expressions such as
+print( age = 5)
+#here print is a statement and age + 3 is expression
+
+# 7 comment : comments are used to explain code. python ignores comment because they are not instructions
+# everything after a hash (#) is considered as comment and is ignored by python
